f8380ce5ef34f962fc2503b283ce8567e5772aa1
ragod123 <35573000+ragod123@users.noreply.github.com>
2024-05-13 12:34:16 +0530

    Update tracking.php

--- demo/tracking.php
+++ demo/tracking.php
@@ -1,13 +1,25 @@
 <?php
 
+use RahulGodiyal\PhpUpsApiWrapper\Entity\TrackingQuery;
 use RahulGodiyal\PhpUpsApiWrapper\Tracking;
 
 require_once('./vendor/autoload.php');
 
-$client_id = "xxxxxxxxxxxxxxxx"; // UPS Client ID
-$client_secret = "xxxxxxxxxxxxxxx"; // UPS Client Secret
+$client_id = "******************************"; // UPS Client ID
+$client_secret = "*****************************************"; // UPS Client Secret
 
-$trackingRes = Tracking::setTrackingNumber("123456789")->fetch($client_id, $client_secret); // For Dev Api
-$trackingRes = Tracking::setTrackingNumber("123456789")->setMode('PROD')->fetch($client_id, $client_secret); // For PROD Api
+/********* Tracking Query *********/
+$query = new TrackingQuery(); // optional
+$query->setLocale("en_US"); // optional
+$query->setReturnSignature("false"); //optional
+$query->setReturnSignature("false"); //optional
+$query->setReturnSignature("false"); //optional
+/********* End Tracking Query *********/
+
+$tracking = new Tracking();
+$trackingRes = $tracking
+    ->setQuery($query) // optional
+    // ->setMode('PROD') // optional
+    ->setTrackingNumber("123456789")->fetch($client_id, $client_secret);
 
 echo '<pre>'; print_r($trackingRes); echo '</pre>'; die();
