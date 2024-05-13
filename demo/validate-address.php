def1cce1b30cc06a730966fd7b2591366630a532
ragod123 <35573000+ragod123@users.noreply.github.com>
2024-05-13 12:31:31 +0530

    Update validate-address.php

--- demo/validate-address.php
+++ demo/validate-address.php
@@ -1,25 +1,36 @@
 <?php
 
-use RahulGodiyal\PhpUpsApiWrapper\AddressValidation;
+use RahulGodiyal\PhpUpsApiWrapper\Entity\ValidateAddressQuery;
+use RahulGodiyal\PhpUpsApiWrapper\ValidateAddress;
 
 require_once('./vendor/autoload.php');
 
-$client_id = "xxxxxxxxxxxxxxxx"; // UPS Client ID
-$client_secret = "xxxxxxxxxxxxxxx"; // UPS Client Secret
+$client_id = "******************************"; // UPS Client ID
+$client_secret = "*****************************************"; // UPS Client Secret
 
-// Address to be validated
-$address = [
-    "AddressLine" => [
-        "785 GODDARD CT"
-    ],
-    "PoliticalDivision2" => "ALPHARATTA",
-    "PoliticalDivision1" => "CA",
-    "PostcodePrimaryLow" => "30005",
-    "CountryCode" => "US"
-];
+
+/********* Query *********/
+$query = new ValidateAddressQuery(); // optional
+$query->setRegionalRequestIndicator("False"); // optional
+$query->setMaximumCandidateListSize("1"); //optional
+/********* End Query *********/
+
+/******** Set Address ********/
+$validateAddress = new ValidateAddress();
+$validateAddress->setQuery($query); // optional
+$validateAddress->setAddressLines([
+    "8001 S Orange Blossom Trl", // address line 1
+    "SPACE K113iokio" // address line 2
+]);
+$validateAddress->setPoliticalDivision2("Orlando"); // City
+$validateAddress->setPoliticalDivision1("FL"); // State Code
+$validateAddress->setPostcodePrimaryLow("32809"); // Postal Code
+$validateAddress->setCountryCode("US");
+/******** End Set Address ********/
+
+$validateAddress->setMode('PROD'); // optional
 
 echo '<pre>';
-print_r(AddressValidation::setAddress($address)->validate($client_id, $client_secret)); // For Dev Api
-print_r(AddressValidation::setAddress($address)->setMode('PROD')->validate($client_id, $client_secret)); // For Prod Api
+print_r($validateAddress->validate($client_id, $client_secret));
 echo '</pre>';
 die();
